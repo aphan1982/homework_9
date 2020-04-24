a3647b529399f0cec4e1890246c8273e3c9226ad
aphan <phantasticvs@yahoo.com>
2020-04-24 16:32:52 -0600

    Unsure how to use 'when' in Inquirer.js to create a conditional. Will flesh out rest of prompts and revisit.

--- index.js
+++ index.js
@@ -10,15 +10,29 @@ inquirer
     {
       type: "confirm",
       message: "Do you have a graphic you'd like to upload for your badge?",
-      name: "badge",
+      name: "customBadge",
       default: false
+      // use WHEN (research documentation) to set up a conditional to determine if a custom or stock badge should be used for README
     },
     {
       type: "input",
       message: "What is the title of your project?",
       name: "projectName"
     },
-    
+    {
+      type: "input",
+      message: "Please give a brief description of your project.",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "If applicable, please provide a table of contents.",
+      name: "tableOfContents"
+    },
+    {
+      type: "input",
+
+    }
   ]);
 
 // This is a good resource for badge generation: https://shields.io/
