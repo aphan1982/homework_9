c6bc88032ac6293d5b34442c7afa9b1790102748
aphan <phantasticvs@yahoo.com>
2020-04-24 16:16:06 -0600

    Installed Inquirer.js. Began creation of prompts: GitHub username, confirmation of badge graphics, project name, and  project description.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    { 
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "username"
+    },
+    {
+      type: "confirm",
+      message: "Do you have a graphic you'd like to upload for your badge?",
+      name: "badge",
+      default: false
+    },
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "projectName"
+    },
+    
+  ]);
+
 // This is a good resource for badge generation: https://shields.io/
