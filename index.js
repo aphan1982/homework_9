b995af05e7eef987ba945bb59acd2136c122d77d
aphan <phantasticvs@yahoo.com>
2020-04-24 17:26:35 -0600

    Created prompts for all of the questionnaire fields required by the homework.

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ inquirer
       message: "What is your GitHub user name?",
       name: "username"
     },
-    {
-      type: "confirm",
-      message: "Do you have a graphic you'd like to upload for your badge?",
-      name: "customBadge",
-      default: false
-      // use WHEN (research documentation) to set up a conditional to determine if a custom or stock badge should be used for README
-    },
+    // {
+    //   type: "confirm",
+    //   message: "Do you have a graphic you'd like to upload for your badge?",
+    //   name: "customBadge",
+    //   default: false
+    //   // use WHEN (research documentation) to set up a conditional to determine if a custom or stock badge should be used for README
+    // },
     {
       type: "input",
       message: "What is the title of your project?",
@@ -26,12 +26,50 @@ inquirer
     },
     {
       type: "input",
-      message: "If applicable, please provide a table of contents.",
+      message: "Please provide a table of contents.",
       name: "tableOfContents"
     },
     {
       type: "input",
-
+      message: "Please describe the installation method.",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "Please provide usage details.",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "What licenses are associated with your project?",
+      name: "licenses"
+    },
+    {
+      type: "input",
+      message: "Please list all contributors to your project.",
+      name: "contributors"
+    },
+    {
+      type: "input",
+      message: "What tests are associated with this project?",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Please list any answers to frequently asked questions.",
+      name: "freqAskedQuestions"
+    },
+    {
+      type: "confirm",
+      message: "Would you like to include your GitHub profile picture?",
+      name: "profilePic",
+      default: false
+    },
+    {
+      type: "confirm",
+      message: "Would you like to include your GitHub e-mail address?",
+      name: "email",
+      default: false
     }
   ]);
 
